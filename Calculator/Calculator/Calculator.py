96d4bf5dbe1eeca759dc5c2beb57eefa6a6c5ea3
Paul Phillips <stratman1234@gmail.com>
2022-02-11 10:59:58 -0800

    Removed unused files, added method subtract to Calculator, added tests for subtract

--- Calculator/Calculator/Calculator.py
+++ Calculator/Calculator/Calculator.py
@@ -3,3 +3,8 @@ class Calculator() :
         if not isinstance(addend1, int) or not isinstance(addend2, int) :
             raise TypeError(f'arguments must be int.  types are {type(addend1)} and {type(addend2)}')
         return addend1 + addend2
+
+    def subtract(self, minuend, subtrahend) :
+        if not isinstance(minuend, int) or not isinstance(subtrahend, int) :
+            raise TypeError(f'arguments must be int.  types are {type(minuend)} and {type(subtrahend)}')
+        return minuend - subtrahend
