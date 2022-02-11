41c90ee22fed22c7929ada371e64b6e608081e76
Paul Phillips <stratman1234@gmail.com>
2022-02-11 10:14:57 -0800

    Added to function add and added tests for function add

--- Calculator/Calculator/Calculator.py
+++ Calculator/Calculator/Calculator.py
@@ -1,3 +1,5 @@
 class Calculator() :
     def add(self, addend1, addend2) :
+        if not isinstance(addend1, int) or not isinstance(addend2, int) :
+            raise TypeError(f'arguments must be int.  types are {type(addend1)} and {type(addend2)}')
         return addend1 + addend2
--- Calculator/Calculator/test_Calculator.py
+++ Calculator/Calculator/test_Calculator.py
@@ -10,3 +10,23 @@ def test_add_0_1() :
     calculator = Calculator()
     answer = calculator.add(0,1)
     assert answer == 1
+
+def test_add_0_None() :
+    calculator = Calculator()
+
+    answer = None
+    try :
+        answer = calculator.add(0,None)
+    except TypeError :
+        assert True
+    assert answer == None
+
+def test_add_None_0() :
+    calculator = Calculator()
+
+    answer = None
+    try :
+        answer = calculator.add(None, 0)
+    except TypeError :
+        assert True
+    assert answer == None
